dbd9c463e76effd29f986363259a7cb7008f109f
jastylr <jtaylor@faultlinemedia.com>
2014-01-28 09:44:16 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trimplify::Application.config.secret_key_base = 'a673204cfd29993c86bb38590fc2fcfa4b3f2a42accfef6c3bbfb719e3098cae8ee1d1274d4c3b193054ae3b9101af1b2f2603170e0ea61ce0ec935762a4b01c'
